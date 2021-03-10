7ce579c3b4fb97ad00c5807e6848aaac7fd5efd3
Dustin Law <dpl656@mail.usask.ca>
2021-03-09 20:20:53 -0600

    Tool.java, made constructors, and did some tests to ensure that able to getAvailability of tools, and to setAvailability

--- src/Tool.java
+++ src/Tool.java
@@ -1,24 +1,63 @@
-public class Tool {
-    private tool_id;
-    private Boolean availability = true;
+import java.util.HashMap;
+
+public class Tool  {
+
+    private HashMap<String, String> tool_list;
+
+
+    //to create an instance of tools
+    private String toolName;
+    private String tool_id;
+    private boolean availability;
+
+
 
     // is there a standardized way to create ids?
-    public Tool() {
-        // set tool_id
+    public Tool(String name, String id,  boolean isavailable) {
+        this.toolName = name;
+        this.tool_id=id;
+        // this.tool_id = tool;
+        this.availability = isavailable;
+
+
     }
 
-    // get tool_id
+
+
+    /**
+     * A method that will get the tool id
+     * @return A string that is the tool_id
+     */
     public String getToolID() {
         return this.tool_id;
     }
 
     // true if available
-    public Boolean getAvailability() {
+    public boolean getAvailability(boolean availability) {
         return this.availability;
     }
 
-    // maybe better param name
-    public void setAvailability(Boolean is_availability) {
-        this.availability = new_availability;
+    /**
+     * A method that will set if a tool is available or not
+     * @param is_available A boolean value that will test if the tool is availlable or not
+     */
+    public void setAvailability(boolean is_available) {
+        this.availability = is_available;
     }
+
+
+    public static void main(String[]args)
+    {
+        Tool tool = new Tool("hammer", "123456", false);
+
+
+        System.out.println("Is the hammer available? :Expect false, and returned: " + tool.getAvailability(tool.availability) + ".");
+        tool.setAvailability(true);
+        System.out.println("Is the hammer available? :Expect true, and returned: " + tool.getAvailability(tool.availability) + ".");
+
+        System.out.println(tool.getToolID());
+
+    }
+
+
 }
