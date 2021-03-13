e5b3e5d4b279d4aefb8de709eaf67dd72505511a
Dustin Law <dpl656@mail.usask.ca>
2021-03-13 11:47:46 -0600

    Updated constructor for Dates

--- src/Contract.java
+++ src/Contract.java
@@ -1,13 +1,14 @@
 
 import java.util.*;
 
+
+
 public class Contract {
     private String contract_id;
     private String tool_id; // of the borrowed tool
     private String employee_id; // of employee borrowing tool
     private Date date_borrowed;
     private Date due_date;
-
 
 
     public Contract(String contract, String t_id, String emp_id, Date d_borrowed, Date due_on) {
@@ -17,18 +18,17 @@ public class Contract {
         this.tool_id = t_id;
         this.employee_id = emp_id;
         //Not sure if we need these two in the constructor or not
-        this.date_borrowed = d_borrowed;
-        this.due_date = due_on;
+        this.date_borrowed = new Date(System.currentTimeMillis());
+        this.due_date = new Date ();
     }
-
 
 
     /**
      * A method that will set the name of the contract
+     *
      * @param contract A string for the name of a contract
      */
-    public void setContract(String contract)
-    {
+    public void setContract(String contract) {
         //sets the contract id
         this.contract_id = contract;
     }
@@ -36,10 +36,10 @@ public class Contract {
 
     /**
      * A method that will get the information of a contract id
+     *
      * @return A string reresenting the contract
      */
-    public String getContract()
-    {
+    public String getContract() {
         return this.contract_id;
     }
 
@@ -48,60 +48,61 @@ public class Contract {
      *
      * @param name A string representing the employee's id
      */
-    public void setEmployee_id(String name)
-    {
+    public void setEmployee_id(String name) {
         this.employee_id = name;
     }
 
     /**
      * A method to get the employee id information from contracts
+     *
      * @return
      */
-    public String getEmployee_id()
-    {
+    public String getEmployee_id() {
         return this.employee_id;
     }
+
     /**
      * A method to find out what date a contract is due by
+     *
      * @return Returns a Date type with the due date
      */
-    public Date getDue_date()
-    {
+    public Date getDue_date() {
         return this.due_date;
     }
 
     /**
      * A method that will set the due date for a contract (i.e. if a due date needs to be extended)
+     *
      * @param date A Date that will be a new due date
      */
-    public void setDue_date(Date date)
-    {
+    public void setDue_date(Date date) {
         this.due_date = date;
     }
 
     /**
      * A method that will get the date the tools were borrowed
+     *
      * @return A Date that represents the date the tool was borrowed (or maybe contract created)
      */
-    public Date getDate_borrowed()
-    {
+    public Date getDate_borrowed() {
         return this.date_borrowed;
     }
 
     /**
      * A method that will set the date a tool was created, or set contract date
+     *
      * @param date A Date representing a date
      */
-    public void setDate_borrowed(Date date)
-    {
+    public void setDate_borrowed(Date date) {
         this.date_borrowed = date;
     }
 
 
     /**
      * A method that will check if the tool is overdue by checking the return date against the checkout date
+     *
      * @param borrowed_date Date: A date that a tool is checked out
-     * @param due_date Date: A dat that the tool is due by
+     * @param due_date      Date: A dat that the tool is due by
      * @return A boolean representing true if the tool is overdue, or false if it is not
      */
     public static boolean isContractOverdue(Date borrowed_date, Date due_date) {
@@ -116,6 +117,7 @@ public class Contract {
     /**
      * A method to get a String representation of the data.
      * *** Needs to be updated if there are multiple employees and/or tool_ids
+     *
      * @return
      */
     public String toString() {
@@ -123,16 +125,15 @@ public class Contract {
     }
 
 
-    public static void main (String[]args)
-    {
+    public static void main(String[] args) {
 
         //create a test to seee if isContractOverdue is doing the right operation
-        Date borrow_date = new Date(2021, 03,15);
+        Date borrow_date = new Date(2021, 03, 15);
         Date signed_out = new Date();
         System.out.println(isContractOverdue(borrow_date, signed_out));
 
 
-        Contract contract = new Contract("test_contract", "12345", "dpl656", new Date (2020,02,12), new Date (2021,03,18) );
+        Contract contract = new Contract("test_contract", "12345", "dpl656", new Date(2020, 02, 12), new Date(2021, 03, 18));
 
         System.out.println(contract.getContract());
         contract.setContract("abc");
@@ -140,3 +141,5 @@ public class Contract {
         System.out.println("test toString:");
         System.out.println(contract.toString());
     }
+
+}
