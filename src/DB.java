f9dbac5622e850fcdf5630d0a4c0b76583d4187d
Dustin Law <dpl656@mail.usask.ca>
2021-03-13 21:50:34 -0600

    DB.java -> updated connectivity for the database to be accessed from a Connection con May need to update the url for the project

--- src/DB.java
+++ src/DB.java
@@ -1,9 +1,31 @@
 package ;
+import java.sql.DriverManager;
+import java.sql.Connection;
 
+/**
+ * A class that will be the connection point to the database
+ */
 public class DB
 {
-    // DB()
-    // init connection
-    // getConnection() { return db connection object for data access classes
-    // }
-}
+    /**
+     * A method that will get a connection to the database
+     * @return A connection to the database will be returned
+     */
+    public static Connection getConnection() {
+        String username = "";
+        String password = "";
+        String url = "jdbc:mysql://localhost:3306/";
+
+        Connection con = null;
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, username, password);
+        }catch (Exception e){System.out.println(e);}
+
+        return con;
+
+
+    }
+
+    }
