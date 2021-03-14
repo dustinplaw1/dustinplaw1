c63769a0de87f56ef1f5bff89d9aa6110dee5754
Dustin Law <dpl656@mail.usask.ca>
2021-03-14 00:49:11 -0600

    ToolManager.java -> cleaned up some constructors, and commented out a method for now

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -1,4 +1,3 @@
-package ;
 
 public class LibrarySystem
 {
--- src/ToolManager.java
+++ src/ToolManager.java
@@ -2,14 +2,16 @@
 
 public class ToolManager extends Employee {
     // maybe use scanner in instead of params?
-    public ToolManager(String emp_id, String f_name, String l_name) {
-        super(emp_id, f_name, l_name);
+
+    public ToolManager(String employee_id, String last_name, String first_name, String employee_type) {
+        super(employee_id, last_name, first_name, employee_type);
     }
     public void addTool(String tool_id) {
 
     }
     // tool_id must exist in the Tool Library tool list
-    public removeTool(String tool_Id) {
 
-    }
+//    public removeTool(String tool_Id) {
+//
+//    }
 }
