04e98f460d6e7662fa97217b6254293e5de531a4
Dustin Law <dpl656@mail.usask.ca>
2021-03-13 23:46:02 -0600

    EmployeeDataAccess.java -> getEmployeeInfo is created.

--- src/EmployeeDataAccess.java
+++ src/EmployeeDataAccess.java
@@ -1,15 +1,39 @@
-package ;
+import java.sql.*;
 
 public class EmployeeDataAccess
 {
-    // public EmployeeDataAccess()
-    /*
-    public listOfAllEmployees getEmployeeList() {
-        return listOfAllEmployees;
-    }
-    */
-    // return type?
+
+
+
+
+
+
+    /**
+     * A method that will display information of an employee
+     * @param employee_id A string representing an employee id to retrieve info
+     * @return
+     */
     public String getEmployeeInfo(String employee_id) {
-        return "";
+        String result ="" ;
+        //try/catch when connecting to a database
+        try{
+            //create a connection
+            Connection con = DB.getConnection();
+            //easier more efficient at accessing data
+            PreparedStatement p = con.prepareStatement(("select employee_id, last_name, first_name, phone_number, employee_type from employee"));
+
+            //cursor for going through data in database
+            ResultSet r = p.executeQuery();
+            //build a string for the employee information
+            result +="Employee name: " + r.getString("last_name") + ", " + r.getString("first_name") +
+            ", phone number: " + r.getString("phone_number") + ", employee type: " + r.getString("employee_type");
+
+
+
+
+        } catch (Exception e) { System.out.println(e);}
+        return result;
+
+
     }
 }
