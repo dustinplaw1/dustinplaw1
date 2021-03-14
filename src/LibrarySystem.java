ce9750433587a6e4da113aebc2a78d0368a55520
Dustin Law <dpl656@mail.usask.ca>
2021-03-14 11:41:07 -0600

    LibrarySystem.java -> created java docs and method stubs

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -4,4 +4,61 @@ public class LibrarySystem
     // Ask user for inputs, start by validating login info, then create employee object of type labourer or tool manager
     // then get the action list for labourer or tool manager
     //
+
+
+    /**
+     * A method that will check if the employee is validated
+     * @param id A string for the employees id
+     * @param password A string for the employees password
+     */
+    public void login(String id, String password){
+
+    }
+
+    /**
+     * A method that will delete a tool from the system
+     * @param id A string that is the tool id
+     */
+    public void deleteTool(String id)
+    {
+
+    }
+
+
+    /**
+     * A method that will create a new tool for use by users
+     * @param tool_id A string tool id
+     * @param tool_name A string name for the tool
+     * @param tool_desc A string for a brief description of the tool
+     */
+    public void createTool(String tool_id, String tool_name,String tool_desc)
+    {
+
+    }
+
+    /**
+     * A method that will borrow a tool from the system
+     * @param tool_id A string that is the tool id
+     */
+    public void borrowTool(String tool_id)
+    {
+
+    }
+
+
+    /**
+     * A method that will return tools that were borrowed
+     * @param id A string with the tool id
+     */
+    public void returnTool(String id)
+    {
+
+    }
+
+
+
+
+
+
+
 }
