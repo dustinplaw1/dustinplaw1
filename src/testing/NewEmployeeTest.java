c6f7332e4b2e95c727b957997d07867cf2cfb745
Dustin Law <dpl656@mail.usask.ca>
2021-03-31 21:40:43 -0600

    NewEmployeeTest.java -> Test Suite for Dustin Law

--- src/testing/NewEmployeeTest.java
+++ src/testing/NewEmployeeTest.java
@@ -6,7 +6,7 @@ import gateways.NewEmployee;
 /**
  * Class for testing the NewEmployee gateway
  */
-public class NewEmployeeTest {
+public class NewEmployeeTest extends TestGateway {
 
     private NewEmployee employee;
 
@@ -23,7 +23,56 @@ public class NewEmployeeTest {
         String first = "John";
         String pass = "abc123";
         String employee_type = "Labourer";
-        employee = new NewEmployee(last, first, pass, employee_type);
+
+
+        try {
+            employee = new NewEmployee(last, first, pass, employee_type);
+        } catch (Exception e) {
+            System.out.println("Failed to initialize tests: \n" + e);
+            System.exit(1);
+        }
+
+        //get database connection
+        try {
+            this.getConnection();
+        } catch (Exception e) {
+            System.out.println("Error making connection to the database");
+        }
+    }
+
+        //New Employee
+        private void Test1()
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
