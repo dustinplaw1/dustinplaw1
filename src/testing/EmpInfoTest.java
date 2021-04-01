158d845f344a509a23ccd62e008266f823c04016
Daniel Stamper (dgs084) <dgs084@mail.usask.ca>
2021-04-01 15:52:20 -0600

    fixed EmpInfoTest so that it will compile

--- src/testing/EmpInfoTest.java
+++ src/testing/EmpInfoTest.java
@@ -29,29 +29,26 @@ public class EmpInfoTest extends TestGateway {
     }
 
     private void Test1 () {
+        this.testStartPrinter(1);
         int testCount = 0;
         try {
             testCount = this.countRows(table);
             emp.execute();
 
-            Checking the Class
             if (this.countRows("employees") != testCount) {
-                testPrinter(1, true);
+                testResultPrinter(1, true);
 
-                this.deleteItem(table, id,);
             } else {
                 throw new Exception("Row Count In employees Unchaged");
             }
         } catch (Exception e) {
-            testPrinter(1, false);
+            testResultPrinter(1, false);
             System.out.println(e);
         }
     }
 
     private void Test2 () {
-        System.out.println("\nRunning: Test 2 of "+total);
-
-
+        this.testStartPrinter(2);
         try {
             emp.execute();
         } catch (Exception e) {
