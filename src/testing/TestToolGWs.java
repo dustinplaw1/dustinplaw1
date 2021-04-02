d409ec35a70289d17e1b646c4a7344447f473c23
Matthew <mds569@usask.ca>
2021-04-01 18:04:11 -0600

    Started TestToolGWs (first test completed)

--- src/testing/TestToolGWs.java
+++ src/testing/TestToolGWs.java
@@ -19,12 +19,35 @@ public class TestToolGWs extends TestGateway
         }
     }
 
-    private void test()
+    private void test1()
     {
+        int numrowsinitial = countRows("tools");
+        int passcount = 0;
+        boolean passfail;
         this.testStartPrinter(1);
-        ct = new CreateTool('test hammer', 'a ballpean hammer');
+        try {
+            ct = new CreateTool("test hammer", "a ballpean hammer");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String num = ct.getTool_id();
+
         try {
             ct.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        passcount++;
+
+        int numrows1 = countRows("tools");
+        if (numrowsinitial == numrows1){
+            passcount = passcount - 1;
+            System.out.println("CreateTool did not add a new tool to the database!");
+        }
+
+        try {
+            dt = new DeleteTool(num);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -34,13 +57,30 @@ public class TestToolGWs extends TestGateway
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        passcount++;
+
+        int numrows2 = countRows("tools");
+        if (numrowsinitial != numrows2){
+            passcount = passcount - 1;
+            System.out.println("DeleteTool did not delete the specified tool from the database!");
+        }
+
+        if (passcount == 2){
+            passfail = true;
+        }
+        else {
+            passfail = false;
+        }
+
+        testResultPrinter(1, passfail);
     }
 
 
     public static void main(String args[])
     {
         TestToolGWs test = new TestToolGWs();
-        test.test();
+        test.test1();
     }
 
 }
